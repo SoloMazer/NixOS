e0ac75318190cda49748d14b4e1ccb452893af63
SoloMazer <solomazer@gmail.com>
2025-06-12 12:41:32 +0530

    inital commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
+
