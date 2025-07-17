9e2730637b36be1b09d65d75e5ace0eff43b1dd3
SoloMazer <solomazer@proton.me>
2025-07-17 08:53:57 +0530

    make specialization for gnome & cosmic

--- system/cosmic.nix
+++ system/cosmic.nix
@@ -1,10 +1,12 @@
 {pkgs, ...}: {
-  services.desktopManager.cosmic.enable = true;
-  services.desktopManager.cosmic.xwayland.enable = true;
-  services.displayManager.cosmic-greeter.enable = false;
-  environment.cosmic.excludePackages = with pkgs; [
-    cosmic-player
-    cosmic-term
-    cosmic-store
-  ];
+  specialisation.cosmic.configuration = {
+    services.desktopManager.cosmic.enable = true;
+    services.desktopManager.cosmic.xwayland.enable = true;
+    services.displayManager.cosmic-greeter.enable = true;
+    environment.cosmic.excludePackages = with pkgs; [
+      cosmic-player
+      cosmic-term
+      cosmic-store
+    ];
+  };
 }
--- system/gnome.nix
+++ system/gnome.nix
@@ -1,25 +1,27 @@
 {pkgs, ...}: {
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-  environment.gnome.excludePackages = with pkgs; [
-    gnome-tour
-    gnome-photos
-    gnome-contacts
-    gnome-weather
-    gnome-maps
-    epiphany
-    geary
-    decibels
-    snapshot
-    gnome-characters
-    file-roller
-    gnome-font-viewer
-    gnome-music
-    totem
-    simple-scan
-    gnome-connections
-    evince
-    yelp
-    gnome-system-monitor
-  ];
+  specialisation.gnome.configuration = {
+    services.displayManager.gdm.enable = true;
+    services.desktopManager.gnome.enable = true;
+    environment.gnome.excludePackages = with pkgs; [
+      gnome-tour
+      gnome-photos
+      gnome-contacts
+      gnome-weather
+      gnome-maps
+      epiphany
+      geary
+      decibels
+      snapshot
+      gnome-characters
+      file-roller
+      gnome-font-viewer
+      gnome-music
+      totem
+      simple-scan
+      gnome-connections
+      evince
+      yelp
+      gnome-system-monitor
+    ];
+  };
 }
